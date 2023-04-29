4272635f29cd8a89d2f19756dcde2e7ef0e7c615
Rafael-monteiro <rafaelmonte46@gmail.com>
2023-04-29 00:44:35 -0300

    Alterando entre musicas

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,2 +1,6 @@
-pub const CLIENT_SECRET: &str="";
 pub const CLIENT_ID: &str="";
+pub const CLIENT_SECRET: &str="";
+pub const TOKEN_URL: &str="https://accounts.spotify.com/api/token";
+pub const PLAYER_HANDLING_URL: &str="https://api.spotify.com/v1/me/player";
+pub const TRACK_CHANGE_COMMANDS: [&str; 2]=["next", "previous"];
+pub const CURRENT_TRACK_COMMANDS: [&str; 3]=["pause", "resume", "repeat"];
---
+++ src/connectors/current_track_connector.rs
@@ -0,0 +1,5 @@
+use std::io::ErrorKind;
+
+pub fn change_current_track(command: &str) -> Result<(), ErrorKind> {
+    todo!("Função ainda não disponível");
+}
